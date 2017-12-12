8a7ea5ba958d057de9974e9ad62b47969762130c
LAB_COM-MT_1 <sasiwan359211110033@gmail.com>
2017-12-12 11:00:57 +0700

    TestWhileLoop

---
+++ src/oop_lab3/TestWhileLoop.java
@@ -0,0 +1,39 @@
+package oop_lab3;
+
+public class TestWhileLoop {
+    public static void main (String[] args){
+        //while
+       // int i = 1;
+       // while (i<=10){
+           // System.out.print(i+" ");
+          //  i++;
+          //  //while
+      //  }
+        //int i = 1;
+       // while (i<=50) {
+        //    System.out.print(i + " ");
+        //    if (i % 10 == 0)
+           //     System.out.print("Hello");
+         //   i++;
+       // }
+        //do-while
+       // System.out.println("\n");
+       // int j=1;
+      //  do {
+       //     System.out.print(j + " ");
+       //     j++;
+       // }while (j<=10);
+        System.out.println("\n");
+        int j=1;
+        do {
+            System.out.print(j + " ");
+            if (j%2 !=0)
+                System.out.print("Hello ");
+            j++;
+        }while (j<=10);
+
+
+
+
+    }//main
+}//class
