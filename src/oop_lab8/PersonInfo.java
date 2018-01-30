35731ed36b46de7249c1316c2667b32459f91dc4
LAB_COM-MT_1 <sasiwan359211110033@gmail.com>
2018-01-30 10:31:57 +0700

    Empoloyee

---
+++ src/oop_lab8/PersonInfo.java
@@ -0,0 +1,23 @@
+package oop_lab8;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        Student student = new Student(
+                "1939900299434",
+                "sasiwan",
+                "Fermale",
+                "21",
+                "359211110033",
+                "IS Management Technology");
+        System.out.println(student.toString());
+
+        System.out.println(student.getName());
+
+    }
+    {
+        Employee employee = new Employee("1234","poiatkan","20000" );
+       System.out.println(employee.toString());
+
+
+    }
+}
