70efed1aaaa55d2856066c6ce676e3732912f61b
LAB_COM-MT_1 <sasiwan359211110033@gmail.com>
2018-01-30 09:39:58 +0700

    Person

---
+++ src/oop_lab8/Student.java
@@ -0,0 +1,42 @@
+package oop_lab8;
+//person is super-class
+//Studen is sub-class
+
+public class Student extends person {
+    private String StudentID;
+    private String major;
+
+    public Student(String personID, String name, String gender, String age, String studentID, String major) {
+
+        super(personID, name, gender, age);
+        StudentID = studentID;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "StudentID='" + StudentID + '\'' +
+                ", major='" + major + '\'' +
+                "} " + super.toString();
+    }
+
+    public String getStudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(String studentID) {
+        StudentID = studentID;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
+
+
+
